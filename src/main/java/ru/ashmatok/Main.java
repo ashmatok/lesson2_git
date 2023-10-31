be47e120161b488fad41421166522f5a3869b97d
Shmatok Anton <ashmatok@gmail.com>
2023-10-31 23:09:10 +0300

    second  message

--- src/main/java/ru/ashmatok/Main.java
+++ src/main/java/ru/ashmatok/Main.java
@@ -6,6 +6,7 @@ package ru.ashmatok;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello QA GURU!");
-        
+        System.out.print("Second text");
+
     }
 }
