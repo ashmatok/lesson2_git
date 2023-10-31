07925284407394589f6b6d39de3b32ede0a6e9db
Shmatok Anton <ashmatok@gmail.com>
2023-10-31 21:56:16 +0300

    init comm

---
+++ src/main/java/ru/ashmatok/Main.java
@@ -0,0 +1,10 @@
+package ru.ashmatok;
+
+/**
+ * Created by anton on 31.10.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
