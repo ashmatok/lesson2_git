13dadd113cb54cde5aa08fb78a8412410676eec0
Shmatok Anton <ashmatok@gmail.com>
2023-10-31 23:11:49 +0300

    commit from master

--- src/main/java/ru/ashmatok/Main.java
+++ src/main/java/ru/ashmatok/Main.java
@@ -5,6 +5,6 @@ package ru.ashmatok;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! HellOOOOOOOOOOO");
     }
 }
